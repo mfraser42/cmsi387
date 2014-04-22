637a82011ec892d90429ef753a626d512c828901
Michael <mfraser702@gmail.com>
2014-04-21 20:11:41 -0700

    completed paged memory address translation

--- homework/paged-memory-address-translation/addressTranslation.c
+++ homework/paged-memory-address-translation/addressTranslation.c
@@ -13,5 +13,21 @@ void setPageTable(pagetable *pt) {
 }
 
 int getPhysical(int logical) {
+    // check if the logical page value exists
+    if ( logical < 0 || logical >= 256 ) {
+        return ERR_OUT_OF_RANGE;
+    }
     
+    // get bits corresponding to the page
+    int page = (( logical & PAGEMASK) >> PAGEBITS);
+    // get bits corresponding to the offset
+    int offset = (logical & PAGESIZE);
+    
+    // check if the page is valid
+    if (ptr[page].valid == 0) {
+        return ERR_INVALID;
+    }
+    
+    // return the page address + the offset
+    return ((ptr[page].frame) << PAGEBITS) + offset;
 }
